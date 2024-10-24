eee98e74573e08486c401621ef5b398d8cbfc509
jzscsaba <johanyak.csaba@nje.hu>
2024-10-24 12:29:56 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CnLINQ_SetOperations
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> A = new List<int> { 1, 2, 3, 4, 5, 6 };
+			List<int> B = new List<int> { 1, 2, 3, 7, 8, 9 };
+			Console.WriteLine("A= " + A.Aggregate("", (a, b) => a + b + ", "));
+			Console.WriteLine("B= " + B.Aggregate("", (a, b) => a + b + ", "));
+			Console.WriteLine("A Union B= " + 
+				A.Union(B).Aggregate("", (a, b) => a + b + ", "));
+			Console.WriteLine("A Intersect B= " + 
+				A.Intersect(B).Aggregate("", (a, b) => a + b + ", "));
+			A.Reverse();
+			Console.WriteLine("Reverse A= " + A.Aggregate("", (a, b) => a + b + ", "));
+		}
+
+	}
+
+}
